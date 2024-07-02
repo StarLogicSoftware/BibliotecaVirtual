9bfd4590eb9ead95b493d62f0e681ee877b24468
santiago coulchinsky <scoulchinsky@gmail.com>
2024-07-02 20:18:24 -0300

    rev

--- BibliotecaVirtual.Consola/Modelos/Prestamo.cs
+++ BibliotecaVirtual.Consola/Modelos/Prestamo.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace BibliotecaVirtual.Consola.Modelos
 {
     public class Prestamo
     {
+        public int LectorId { get; set; }
+        public Lector Lector { get; set; }
+        public int LibroId { get; set; }
+        public Libro Libro { get; set; }
+
     }
 }
---
+++ BibliotecaVirtual.Consola/Vistas/LibroVista.cs
@@ -0,0 +1,22 @@
+﻿using BibliotecaVirtual.Consola.Modelos;
+using BibliotecaVirtual.Consola.Repositorio;
+
+namespace BibliotecaVirtual.Consola.Vistas
+{
+    public class LibroVista
+    {
+        public void CrearLibro()
+        {
+            var repo = new RepoGenerico<Libro>();
+
+            Console.Write("Titulo: ");
+            string titulo = Console.ReadLine();
+            Console.WriteLine();
+
+            var libro = new Libro() { Titulo = titulo };
+
+            repo.Crear(libro);
+        }
+
+    }
+}
